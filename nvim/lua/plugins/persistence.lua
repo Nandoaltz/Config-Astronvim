27f917ffb912285d85a634f3a6c1fbbb8ab40646
Nando <102630615+Nandoaltz@users.noreply.github.com>
2025-04-06 21:00:34 -0300

    Update persistence.lua

--- nvim/lua/plugins/persistence.lua
+++ nvim/lua/plugins/persistence.lua
@@ -1,9 +1,8 @@
 return {
-    "folke/persistence.nvim",
-    event = "BufReadPre", 
-    opts = {
-      dir = vim.fn.stdpath("state") .. "/sessions/",
-      options = { "buffers", "curdir", "tabpages", "winsize" },
-    },
-  }
-  
+  "folke/persistence.nvim",
+  event = "BufReadPre", -- ou "VeryLazy"
+  opts = {
+    dir = vim.fn.stdpath("state") .. "/sessions/",
+    options = { "buffers", "curdir", "tabpages", "winsize" },
+  },
+}
