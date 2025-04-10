f1cba92f2f9f93b28f3ea670f4b751a536106d40
Nando <102630615+Nandoaltz@users.noreply.github.com>
2025-04-10 16:07:33 -0300

    Update lualine.lua

--- nvim/lua/user/plugins/lualine.lua
+++ nvim/lua/user/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
         local function lsp_name()
-            local clients = vim.lsp.get_active_clients({ bufnr = 0 })
+            local clients = vim.lsp.get_clients({ bufnr = 0 })
             if next(clients) == nil then
                 return " "
             end
@@ -13,23 +13,60 @@ return {
             end
             return " " .. table.concat(names, ", ")
         end
+        theme = require('lualine.themes.auto')
+theme.normal.c.bg = '#1e1e2e' 
 
-        require("lualine").setup({
+theme.command = {
+  a = { bg = '#c24040', fg = '#1e1e2e', gui = 'bold' },
+}
+
+theme.normal.a.bg = '#c24040'
+theme.normal.z.bg = '#c24040'
+
+theme.insert.a.bg = '#c24040' -- cor de fundo da seção 'a' em modo insert
+theme.visual.a.bg = '#c24040' -- cor de fundo da seção 'a' em modo visual
+theme.replace.a.bg = '#c24040'
+
+        require('lualine').setup {
             options = {
-                theme = "palenight",
-                disabled_filetypes = {
-                    statusline = { "NvimTree", "alpha" },
-                    winbar = {},
-                },
+              icons_enabled = true,
+              theme = theme,
+              component_separators = { left = '', right = ''},
+              section_separators = { left = '', right = ''},
+              disabled_filetypes = {
+                statusline = { "NvimTree", "alpha" },
+                winbar = {},
+              },
+              ignore_focus = {},
+              always_divide_middle = true,
+              always_show_tabline = true,
+              globalstatus = false,
+              refresh = {
+                statusline = 100,
+                tabline = 100,
+                winbar = 100,
+              }
             },
             sections = {
-                lualine_a = { "mode" },
-                lualine_b = { "branch" },
-                lualine_c = { "filename" },
-                lualine_x = { lsp_name, "encoding", "fileformat", "filetype" },
-                lualine_y = { "progress" },
-                lualine_z = { "location" },
+              lualine_a = {'mode'},
+              lualine_b = {'branch', 'diff', 'diagnostics'},
+              lualine_c = {'filename'},
+              lualine_x = {lsp_name, 'filetype'},
+              lualine_y = {'progress'},
+              lualine_z = {'location'}
             },
-        })
+            inactive_sections = {
+              lualine_a = {},
+              lualine_b = {},
+              lualine_c = {'filename'},
+              lualine_x = {'location'},
+              lualine_y = {},
+              lualine_z = {}
+            },
+            tabline = {},
+            winbar = {},
+            inactive_winbar = {},
+            extensions = {}
+          }
     end,
 }
