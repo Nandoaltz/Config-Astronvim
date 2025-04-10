23cc90dd8495c955c449655ddb1c3dc12ab35b1b
Nando <102630615+Nandoaltz@users.noreply.github.com>
2025-04-10 16:59:28 -0300

    Update cmp.lua

--- nvim/lua/user/plugins/cmp.lua
+++ nvim/lua/user/plugins/cmp.lua
@@ -1,52 +1,85 @@
 return {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
     dependencies = {
-        "hrsh7th/cmp-buffer", -- source for text in buffer
-        "hrsh7th/cmp-path", -- source for file system paths
-        {
-            "L3MON4D3/LuaSnip",
-            version = "v2.*",
-            -- install jsregexp (optional!).
-            build = "make install_jsregexp",
-        },
-        "rafamadriz/friendly-snippets",
-        "onsails/lspkind.nvim", -- vs-code like pictograms
+        "hrsh7th/cmp-nvim-lsp",
+        { "folke/neodev.nvim", opts = {} },
     },
     config = function()
-        local cmp = require("cmp")
-        local lspkind = require("lspkind")
-        local luasnip = require("luasnip")
+        local nvim_lsp = require("lspconfig")
+        local mason_lspconfig = require("mason-lspconfig")
 
-        require("luasnip.loaders.from_vscode").lazy_load()
+        local protocol = require("vim.lsp.protocol")
 
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    luasnip.lsp_expand(args.body)
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-Space>"] = cmp.mapping.complete(),
-                ["<C-e>"] = cmp.mapping.close(),
-                ["<CR>"] = cmp.mapping.confirm({
-                    behavior = cmp.ConfirmBehavior.Replace,
-                    select = true,
-                }),
-            }),
-            sources = cmp.config.sources({
-                { name = "nvim_lsp" },
-                { name = "luasnip" },
-                { name = "buffer" },
-                { name = "path" },
-            }),
+        local on_attach = function(client, bufnr)
+            -- format on save
+            if client.server_capabilities.documentFormattingProvider then
+                vim.api.nvim_create_autocmd("BufWritePre", {
+                    group = vim.api.nvim_create_augroup("Format", { clear = true }),
+                    buffer = bufnr,
+                    callback = function()
+                        vim.lsp.buf.format()
+                    end,
+                })
+            end
+        end
+
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+        mason_lspconfig.setup_handlers({
+            function(server)
+                nvim_lsp[server].setup({
+                    capabilities = capabilities,
+                })
+            end,
+            ["cssls"] = function()
+                nvim_lsp["cssls"].setup({
+                    on_attach = on_attach,
+                    capabilities = capabilities,
+                })
+            end,
+            ["gopls"] = function()
+                nvim_lsp["jdtls"].setup({
+                    on_attach = on_attach,
+                    capabilities = capabilities,
+                })
+            end,   
+            ["jdtls"] = function()
+                nvim_lsp["jdtls"].setup({
+                    on_attach = on_attach,
+                    capabilities = capabilities,
+                })
+            end,            
+            ["tailwindcss"] = function()
+                nvim_lsp["tailwindcss"].setup({
+                    on_attach = on_attach,
+                    capabilities = capabilities,
+                })
+            end,
+            ["html"] = function()
+                nvim_lsp["html"].setup({
+                    on_attach = on_attach,
+                    capabilities = capabilities,
+                })
+            end,
+            ["jsonls"] = function()
+                nvim_lsp["jsonls"].setup({
+                    on_attach = on_attach,
+                    capabilities = capabilities,
+                })
+            end,
+            ["eslint"] = function()
+                nvim_lsp["eslint"].setup({
+                    on_attach = on_attach,
+                    capabilities = capabilities,
+                })
+            end,
+            ["pyright"] = function()
+                nvim_lsp["pyright"].setup({
+                    on_attach = on_attach,
+                    capabilities = capabilities,
+                })
+            end,
         })
-
-        vim.cmd([[
-      set completeopt=menuone,noinsert,noselect
-      highlight! default link CmpItemKind CmpItemMenuDefault
-    ]])
     end,
 }
