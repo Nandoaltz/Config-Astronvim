cd005972db789b3bb2a5dc4eaf7735653dbfba23
Nando <102630615+Nandoaltz@users.noreply.github.com>
2025-04-11 12:36:44 -0300

    Update lualine.lua

--- nvim/lua/user/plugins/lualine.lua
+++ nvim/lua/user/plugins/lualine.lua
@@ -49,7 +49,14 @@ theme.replace.a.bg = '#c24040'
             },
             sections = {
               lualine_a = {'mode'},
-              lualine_b = {'branch', 'diff', 'diagnostics'},
+               lualine_b = {
+                {
+                  'branch',
+                  icon = ''
+                },
+                'diff',
+                'diagnostics'
+              },
               lualine_c = {'filename'},
               lualine_x = {lsp_name, 'filetype'},
               lualine_y = {'progress'},
