1be6be9a0e4495beb5350b192a1626388d6892c8
Nando <102630615+Nandoaltz@users.noreply.github.com>
2025-04-12 13:36:52 -0300

    Update dashboard.lua

--- nvim/lua/user/plugins/dashboard.lua
+++ nvim/lua/user/plugins/dashboard.lua
@@ -16,20 +16,6 @@ vim.api.nvim_create_autocmd("User", {
     opts = function(_, _)
       local dashboard = require("alpha.themes.dashboard")
   
-      -- AutoCmds para ocultar barra de status e tabline no Alpha
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "AlphaReady",
-        callback = function()
-          vim.cmd [[ set laststatus=0 | set showtabline=0 ]]
-        end,
-      })
-      vim.api.nvim_create_autocmd("BufUnload", {
-        buffer = 0,
-        callback = function()
-          vim.cmd [[ set laststatus=2 | set showtabline=2 ]]
-        end,
-      })
-  
       -- Logo
       dashboard.section.header.val = {
 [[                                                     ]],
