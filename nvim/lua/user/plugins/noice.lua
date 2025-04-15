c5dcb17757a15467e3a7190fe57903c67e39338c
Nando <102630615+Nandoaltz@users.noreply.github.com>
2025-04-14 23:09:27 -0300

    Update noice.lua

--- nvim/lua/user/plugins/noice.lua
+++ nvim/lua/user/plugins/noice.lua
@@ -1,40 +1,23 @@
-return
-{
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  },
+  opts = {
+    lsp = {
+      signature = { enabled = false },
+      hover = { enabled = false },
     },
-    opts = {
-      cmdline = {
-        view = "cmdline", -- ou "none" para ocultar
-        format = {
-          cmdline = { icon = "", conceal = false },
-          search_down = { icon = " ", conceal = false },
-          search_up = { icon = " ", conceal = false },
+    routes = {
+      {
+        filter = {
+          event = "msg_show",
+          find = "deprecated",
         },
-      },
-      messages = {
-        enabled = true,
-        view = "mini", -- ou "none"
-        view_error = "mini",
-        view_warn = "mini",
-      },
-      notify = {
-        enabled = true,
-        view = "mini",
-      },
-      lsp = {
-        progress = { enabled = false },
-        signature = { enabled = false },
-        hover = { enabled = false },
-      },
-      presets = {
-        bottom_search = true,    -- Clássico estilo de busca do vim
-        command_palette = false, -- Requer configuração
-        long_message_to_split = true,
+        opts = { skip = true },
       },
     },
-  }
-  
+  },
+}
