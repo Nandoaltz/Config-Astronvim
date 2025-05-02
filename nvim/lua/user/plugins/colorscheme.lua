627bbdbeb70cd791f56751af5740e8c0515f03e7
Nando <102630615+Nandoaltz@users.noreply.github.com>
2025-05-02 20:52:31 -0300

    Update colorscheme.lua

--- nvim/lua/user/plugins/colorscheme.lua
+++ nvim/lua/user/plugins/colorscheme.lua
@@ -1,12 +1,12 @@
-return {
-  "tiagovla/tokyodark.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-      vim.cmd("colorscheme tokyodark")
-  end,
-}
-
+--return {
+--  "tiagovla/tokyodark.nvim",
+--  lazy = false,
+--  priority = 1000,
+--  config = function()
+--      vim.cmd("colorscheme tokyodark")
+--  end,
+--}
+    
 --return {
 --    "nyoom-engineering/oxocarbon.nvim",
 --    lazy = false,
@@ -16,11 +16,47 @@ return {
 --    end,
 --}
 
+return {
+    "catppuccin/nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        vim.cmd("colorscheme catppuccin-mocha")
+    end,
+}
+
 --return {
---    "Mofiqul/dracula.nvim",
---    lazy = false,
---    priority = 1000,
---    config = function()
---        vim.cmd("colorscheme dracula")
---    end,
+--  "rebelot/kanagawa.nvim",
+--  lazy = false,
+--  priority = 1000,
+--  config = function()
+--      vim.cmd("colorscheme kanagawa")
+--  end,
+--}
+
+--return {
+--  "Mofiqul/dracula.nvim",
+--  lazy = false,
+--  priority = 1000,
+--  config = function()
+--      vim.cmd("colorscheme dracula")
+--  end,
+--}
+
+--return {
+--  "dasupradyumna/midnight.nvim",
+--  lazy = false,
+--  priority = 1000,
+--  config = function()
+--      vim.cmd("colorscheme midnight")
+--  end,
+--}
+
+--return {
+--  "folke/tokyonight.nvim",
+--  lazy = false,
+--  priority = 1000,
+--  config = function()
+--      vim.cmd("colorscheme tokyonight")
+--  end,
 --}
